4cff701d28c4ada0aabd55cc5c01a9e82f6bd365
Abalfazl <abalfazldev@gmail.com>
2025-06-07 11:43:11 +0330

    fix: print True or False

--- Proj.py
+++ Proj.py
@@ -3,7 +3,7 @@ lst_t = [
     [1, 1, 0, 0, 1, 1],
     [0, 1, 0, 1, 1, 1],
     [0, 1, 0, 1, 1, 1],
-    [1, 1, 0, 0, 0, 1]
+    [1, 1, 1, 0, 0, 1]
 ]
 
 lst_t2 = [
@@ -15,31 +15,33 @@ lst_t2 = [
 ]
 
 
-
 def find_way_in_lst(lst):
     row_len = len(lst)
     col_len = len(lst[0])
+    consist = False
+    checked = [[False for _ in range(col_len)] for _ in range(row_len)]
+    #Print input list:
+    for r in lst:
+        print(r)
 
     def find_way(row, col):
+        nonlocal consist
+        if row_len == row + 1 and lst[row][col] == 0:
+            consist = True
 
-        if row_len - 1 < row:
-            print("row_len is less than row")
-            return True
-
-        if lst[row][col] == 0:
-            find_way(row + 1, col)
-            if col + 1 <= col_len - 1:
+        elif lst[row][col] == 0 and not checked[row][col]:
+            checked[row][col] = True
+            if col + 1 <= col_len - 1 and not checked[row][col + 1]:
                 find_way(row, col + 1)
-            if col - 1 >= 0:
+            if col - 1 >= 0 and not checked[row][col - 1]:
                 find_way(row, col - 1)
-
+            find_way(row + 1, col)
 
     for i in range(col_len):
         if lst[0][i] == 0:
-           if find_way(1, i):
-               return True
-    else:
-        return False
+            find_way(1, i)
+
+    return consist
 
 
-print(find_way_in_lst(lst_t2))
+print(find_way_in_lst(lst_t))
